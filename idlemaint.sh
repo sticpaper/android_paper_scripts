2c360a88ea0eaefada685b6f218abcf901c0d60c
admin <paper@localhost>
2024-05-26 16:13:22 +0800

    idlemaint: Add a trigger GC function based on Android IdleMaint
    * Manually trigger Android IdleMaint to trigger F2FS urgent GC mode
    * At the same time, add a conditional judgment:
      - if there are too few dirty segments, end the script directly.
    * Waiting for results: (if gc_urgent is 0) then IdleMaint ends.
    * To prevent multiple executions in a short period of time,
      add a counting and waiting function, and wait until gc_urgent ends
      before automatically ending the script file.
    
      https://android-review.googlesource.com/c/platform/system/vold/+/1936286
      https://android-review.googlesource.com/c/platform/frameworks/base/+/1936378
    
    Change-Id: Ib9659f44af413ff18334094c5927a5d2ecda5d6e

--- idlemaint.sh
+++ idlemaint.sh
@@ -26,6 +26,7 @@ function amktiao_main()
     echo "[-] 版本: $mversion"
 
     check_run_context
+    start_run_idlemaint
 }
 
 function show_volume_info()
@@ -34,6 +35,29 @@ function show_volume_info()
     target_dirty_segs=$(cat $get_f2fs_sysfs/dirty_segments)
     echo "[-] [$get_data_dev] 目前脏段: $target_dirty_segs"
     echo "[-] [$get_data_dev] 目前空闲段: $target_free_segs"
+}
+
+function start_run_idlemaint()
+{
+    show_volume_info
+    # 当目标磁盘卷的脏段低于 256 跳过 GC
+    if [ ! $(cat $get_f2fs_sysfs/dirty_segments) -ge 256 ]; then
+        echo "[#] [$get_data_dev] 的脏段低于预设值 不需要 GC"
+        exit 0
+    fi
+
+    sm idle-maint run; sleep 2; start_time=$(date +%s)
+    echo "[-] [$get_data_dev] 空闲维护已开始, 请耐心等待"
+
+    # 等待紧急GC节点为0 说明StartGc已完成
+    while [ ! $(cat $get_f2fs_sysfs/gc_urgent) == 0 ]; do
+        cur_time=$(date +%s); run_time=$((cur_time - start_time))
+        echo -ne "[#] [$get_data_dev] 空闲维护 服务已运行 $run_time 秒\r"
+        sleep 1 # 检查循环间隔 1秒
+    done
+
+    echo "[-] [$get_data_dev] 磁盘 空闲维护 已完成, 共耗时 $run_time 秒"
+    show_volume_info
 }
 
 # Software main
