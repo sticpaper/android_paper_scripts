68a5e77de97ce8865d7f6615b1fa4c20c248e61b
admin <paper@localhost>
2024-05-26 15:56:54 +0800

    idlemaint: Create idlemaint.sh file (based on Paper-template)
    Change-Id: I58f55b896bd50da165dc30d0ec8d201d1103894e

---
+++ idlemaint.sh
@@ -0,0 +1,14 @@
+#!/vendor/bin/sh
+# F2FS Urgent GC Trigger Scripts
+# By Amktiao Date: 2024-02-28
+
+mversion="20240526"
+
+function amktiao_main()
+{
+    echo "[-] F2FS 触发 紧急GC 回收小工具"
+    echo "[-] 版本: $mversion"
+}
+
+# Software main
+amktiao_main
